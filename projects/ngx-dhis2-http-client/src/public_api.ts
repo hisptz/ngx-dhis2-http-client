db4bc79d0333723ecc292e9231c53861781876ff
Rajab Mkomwa <rajeyw@gmail.com>
2019-06-27 17:56:43 +0300

    refactor(public_api): expose interface for system info for public use

--- projects/ngx-dhis2-http-client/src/public_api.ts
+++ projects/ngx-dhis2-http-client/src/public_api.ts
@@ -4,5 +4,6 @@ export {
 export { SystemInfoService } from './lib/services/system-info.service';
 export { User } from './lib/models/user.model';
 export { Manifest } from './lib/models/manifest.model';
+export { SystemInfo } from './lib/models/system-info.model';
 export { HttpConfig } from './lib/models/http-config.model';
 export { NgxDhis2HttpClientModule } from './lib/ngx-dhis2-http-client.module';
