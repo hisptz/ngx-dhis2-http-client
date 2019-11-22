51394df7e3f8c3c914fa9db06877058cbd1fbc7f
Rajab Mkomwa <rajeyw@gmail.com>
2019-11-22 12:09:22 +0300

    feat: add default http header option for content type

--- projects/ngx-dhis2-http-client/src/lib/constants/http.constant.ts
+++ projects/ngx-dhis2-http-client/src/lib/constants/http.constant.ts
@@ -1,11 +1,15 @@
 import { HttpConfig } from '../models/http-config.model';
 
 export const HTTP_CONFIG: HttpConfig = {
-  includeVersionNumber: false,
-  preferPreviousApiVersion: false,
-  useRootUrl: false,
-  useIndexDb: false,
-  isExternalLink: false
+    includeVersionNumber: false,
+    preferPreviousApiVersion: false,
+    useRootUrl: false,
+    useIndexDb: false,
+    isExternalLink: false,
 };
 
 export const DEFAULT_ROOT_URL = '../../../';
+
+export const HTTP_HEADER_OPTIONS = {
+    'Content-Type': 'application/json',
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,83 +4,63 @@ import * as _ from 'lodash';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(
-    private httpClient: NgxDhis2HttpClientService,
-    private http: HttpClient
-  ) {
-    httpClient
-      .get(
-        'users.json?fields=id,name,created&paging=false&filter=name:ilike:John',
-        {
-          useIndexDb: true,
-          indexDbConfig: { schema: 'users' }
-        }
-      )
-      .subscribe(
-        res => {
-          console.log(res);
-        },
-        error => {
-          console.log(error);
-        }
-      );
-    httpClient
-      .get('users/rWLrZL8rP3K.json?fields=id,name,created', {
-        useIndexDb: true
-      })
-      .subscribe(
-        res => {
-          console.log(res);
-        },
-        error => {
-          console.log(error);
-        }
-      );
+    constructor(
+        private httpClient: NgxDhis2HttpClientService,
+        private http: HttpClient
+    ) {
+        httpClient
+            .get(
+                'users.json?fields=id,name,created&paging=false&filter=name:ilike:John',
+                {
+                    useIndexDb: true,
+                    indexDbConfig: { schema: 'users' },
+                }
+            )
+            .subscribe(
+                res => {
+                    console.log(res);
+                },
+                error => {
+                    console.log(error);
+                }
+            );
+        httpClient
+            .get('users/rWLrZL8rP3K.json?fields=id,name,created', {
+                useIndexDb: true,
+            })
+            .subscribe(
+                res => {
+                    console.log(res);
+                },
+                error => {
+                    console.log(error);
+                }
+            );
 
-    this.httpClient.me().subscribe(me => {
-      console.log('CURRENT USER: ', me);
-    });
-    this.httpClient.systemInfo().subscribe(info => {
-      console.log('SYSTEM INFO: ', info);
-    });
+        this.httpClient.me().subscribe(me => {
+            console.log('CURRENT USER: ', me);
+        });
+        this.httpClient.systemInfo().subscribe(info => {
+            console.log('SYSTEM INFO: ', info);
+        });
 
-    this.httpClient.manifest().subscribe(manifest => {
-      console.log('MANIFEST: ', manifest);
-    });
-  }
+        this.httpClient.manifest().subscribe(manifest => {
+            console.log('MANIFEST: ', manifest);
+        });
+    }
 
-  onPost(e) {
-    const randomId = _.random(1000, 1000000);
-    e.stopPropagation();
-    // this.httpClient
-    //   .post(`dataStore/http-namespace/${randomId}`, 'string value', {
-    //     httpHeaders: { 'Content-Type': 'text/plain' }
-    //   })
-    //   .subscribe(res => {
-    //     console.log(res);
-    //   });
-
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json;charset=UTF-8'
-      })
-    };
-
-    console.log(`\"Jane Doe, Mike Smith\"`);
-
-    this.http
-      .post(
-        `api/dataStore/http-namespace/${randomId}`,
-        `\"Jane Doe, Mike Smith\"`,
-        httpOptions
-      )
-      .subscribe(res => {
-        console.log(res);
-      });
-  }
+    onPost(e) {
+        const randomId = _.random(1000, 1000000);
+        e.stopPropagation();
+        this.httpClient
+            .post(`dataStore/http-namespace/${randomId}`, 'string value')
+            .subscribe(res => {
+                console.log(res);
+            });
+    }
 }
